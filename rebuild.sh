95e1ce4398a42f0fc4d1e67d73ec90ed04ccb4d5
PEN <uemasaki20011222@gmail.com>
2026-05-02 10:00:57 +0900

    rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -6,5 +6,6 @@ rm -rf ./build/sstate-cache/
 rm -rf ./build/tmp
 rm -rf ./build/images/
 rm -rf ./build/yocto-rootfs.tar.gz
+rm -rf ./build/build_yocto/conf/bblayers.conf
 
 echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns && docker compose up --build -d
